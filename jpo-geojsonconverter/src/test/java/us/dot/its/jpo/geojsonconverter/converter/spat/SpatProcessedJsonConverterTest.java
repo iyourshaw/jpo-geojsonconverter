dbb5f59a944cb2150c3c5115478ee21b798d337e
Michael7371 <40476797+Michael7371@users.noreply.github.com>
2025-12-15 11:58:38 -0700

    adding missing import

--- jpo-geojsonconverter/src/test/java/us/dot/its/jpo/geojsonconverter/converter/spat/SpatProcessedJsonConverterTest.java
+++ jpo-geojsonconverter/src/test/java/us/dot/its/jpo/geojsonconverter/converter/spat/SpatProcessedJsonConverterTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.mock;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.kafka.streams.KeyValue;
